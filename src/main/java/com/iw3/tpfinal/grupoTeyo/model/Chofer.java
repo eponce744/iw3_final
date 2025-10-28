dea9f848652e5c868de5dd2f1e889d7798c64fb3
ErnestoPonce <eponce744@gmail.com>
2025-10-28 18:20:45 -0300

    Entidad chofer

--- src/main/java/com/iw3/tpfinal/grupoTeyo/model/Chofer.java
+++ src/main/java/com/iw3/tpfinal/grupoTeyo/model/Chofer.java
@@ -1,5 +1,29 @@
 package com.iw3.tpfinal.grupoTeyo.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name="choferes")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class Chofer {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(length = 60)
+    private String nombre;
+
+    @Column(length = 60)
+    private String apellido;
+
+    @Column(length = 10, unique = true)
+    private String documento;
 }
