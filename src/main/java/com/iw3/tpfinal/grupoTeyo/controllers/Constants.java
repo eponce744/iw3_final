f4412212ff91b4e0d816e1d9a281394ac2a7cc6a
ErnestoPonce <eponce744@gmail.com>
2025-11-01 20:03:28 -0300

    Clases controller e implementacion del ProductoRestController

---
+++ src/main/java/com/iw3/tpfinal/grupoTeyo/controllers/Constants.java
@@ -0,0 +1,15 @@
+package com.iw3.tpfinal.grupoTeyo.controllers;
+
+public final class Constants {
+    public static final String URL_API = "/api";
+    public static final String URL_API_VERSION = "/v1";
+    public static final String URL_BASE = URL_API + URL_API_VERSION;
+
+    //Servicios
+    public static final String URL_CAMIONES = URL_BASE + "/camiones";
+    public static final String URL_CHOFERES = URL_BASE + "/choferes";
+    public static final String URL_CLIENTES = URL_BASE + "/clientes";
+    public static final String URL_ORDENES = URL_BASE + "/ordenes";
+    public static final String URL_DETALLES = URL_BASE + "/detalles";
+    public static final String URL_PRODUCTOS = URL_BASE + "/productos";
+}
---
+++ src/main/java/com/iw3/tpfinal/grupoTeyo/controllers/OrdenRestController.java
@@ -0,0 +1,11 @@
+package com.iw3.tpfinal.grupoTeyo.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(Constants.URL_ORDENES)
+public class OrdenRestController {
+
+
+}
