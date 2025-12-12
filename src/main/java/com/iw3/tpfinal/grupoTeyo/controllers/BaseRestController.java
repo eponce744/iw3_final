6277bd3054975dc59995fe68edc5703cbb3d697d
Maximo Teyo <maxiiteyo@gmail.com>
2025-12-12 20:54:50 -0300

    baseRest

--- src/main/java/com/iw3/tpfinal/grupoTeyo/controllers/BaseRestController.java
+++ src/main/java/com/iw3/tpfinal/grupoTeyo/controllers/BaseRestController.java
@@ -1,4 +1,14 @@
 package com.iw3.tpfinal.grupoTeyo.controllers;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import com.iw3.tpfinal.grupoTeyo.auth.User;
+
 public class BaseRestController {
+    protected User getUserLogged() {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        User user = (User) auth.getPrincipal();
+        return user;
+    }
 }
