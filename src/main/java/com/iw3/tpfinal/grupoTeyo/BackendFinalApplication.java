592f790d4566e07e7809fa0dd00b9a003244b461
Maximo Teyo <maxiiteyo@gmail.com>
2026-02-09 20:36:14 -0300

    Cambios en backendapplication (para que se ejecute en la vm)

--- src/main/java/com/iw3/tpfinal/grupoTeyo/BackendFinalApplication.java
+++ src/main/java/com/iw3/tpfinal/grupoTeyo/BackendFinalApplication.java
@@ -1,16 +1,26 @@
 package com.iw3.tpfinal.grupoTeyo;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 import lombok.extern.slf4j.Slf4j;
 
 @SpringBootApplication
 @Slf4j
-public class BackendFinalApplication{
+public class BackendFinalApplication extends SpringBootServletInitializer implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendFinalApplication.class, args);
 	}
-	
+
+	@Value("${spring.profiles.active}")
+	private String profile;
+
+	@Override
+	public void run(String... args) throws Exception {
+		log.info("Perfil Activo '{}'", profile);
+	}
 }
